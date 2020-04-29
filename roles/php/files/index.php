4c3cd31ddf0c1edd17c7311265d5f5f772cc84a1
Rodrigo Bossini <professorbossini@gmail.com>
2020-04-29 11:04:35 -0300

    Adicao de um playbook e roles para apache e PHP

---
+++ roles/php/files/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Devops na nuvem da Amazon!</title>
+</head>
+
+<body>
+    <h1>Devops!</h1>
+    <p>
+        <?php echo "Hora atual: " .date("h:i:sa"); ?>
+        
+    </p>
+
+</body>
+
+</html>
